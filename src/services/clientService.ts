fab5ab8d3275d2de5ccc67e2c96d9da30b37782b
matvimykula <matt.pronchick@gmail.com>
2023-03-22 19:40:48 -0700

    Add getClients method to clientService

--- src/services/clientService.ts
+++ src/services/clientService.ts
@@ -2,20 +2,19 @@ import axios from 'axios';
 import Client from '@/types/user';
 import currentEnv from '@/utils/environment';
 
-const getClients = () => {
-  console.log('getting user data');
+const getClients = async () => {
+  const response = await axios.get(`${currentEnv()}/api/clients`);
+  return response.data;
 };
 
 const postClient = (clientData: Client) => {
-  console.log('posting blog post');
   axios
-    .post(currentEnv() + 'postClient', {
+    .post(`${currentEnv()}/api/clients`, {
       clientData,
     })
-    .then((response) => {
-      console.log('this blog post should be posted');
-
-      console.log(response.data);
+    .then((response) => {})
+    .catch((err) => {
+      console.log(err);
     });
 };
 
