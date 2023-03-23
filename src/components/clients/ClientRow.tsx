3e6ed52cb4daf87a6cf0c8cae2aefdd30525244f
matvimykula <matt.pronchick@gmail.com>
2023-03-23 11:53:32 -0700

    Fix display of boolean values

--- src/components/clients/ClientRow.tsx
+++ src/components/clients/ClientRow.tsx
@@ -9,15 +9,15 @@ interface ClientRowProps {
 export default function ClientRow({ key, rowData }: ClientRowProps) {
   console.log(rowData);
   return (
-    <Box key={key}>
-      <tr key={rowData.id}>
-        <td>{rowData.first_name}</td>
-        <td>{rowData.cell}</td> <td>{rowData.email}</td>{' '}
-        <td>{rowData.email_ok}</td> <td>{rowData.cell}</td>{' '}
-        <td>{rowData.text_ok}</td> <td>{rowData.num_sessions}</td>{' '}
-        <td>{rowData.num_cancels}</td> <td>{rowData.rate}</td>{' '}
-        <td>{rowData.payment_method}</td>{' '}
-      </tr>
-    </Box>
+    <tr key={rowData.id}>
+      <td>{rowData.first_name}</td>
+      <td>{rowData.last_name}</td>
+      <td>{rowData.cell}</td>
+      <td>{`${rowData.text_ok}`}</td>
+      <td>{rowData.email}</td> <td>{`${rowData.email_ok}`}</td>
+      <td>{rowData.payment_method}</td> <td>{rowData.num_sessions}</td>{' '}
+      <td>{rowData.num_cancels}</td>
+      <td>{rowData.rate}</td>{' '}
+    </tr>
   );
 }
