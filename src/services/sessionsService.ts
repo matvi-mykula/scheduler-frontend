960f87fbe5fa3289d6539f027df5006d3b5a51af
matvimykula <matt.pronchick@gmail.com>
2023-03-28 19:00:11 -0700

    Add delete and update session services. Also start to figure time conversion...

--- src/services/sessionsService.ts
+++ src/services/sessionsService.ts
@@ -18,4 +18,42 @@ const postSession = (sessionData: Session) => {
     });
 };
 
-export { getSessions, postSession };
+const deleteSession = (sessionData: Session) => {
+  console.log(sessionData);
+  axios
+    .delete(`${serverPath}/api/sessions`, { data: sessionData })
+    .then((response) => {})
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const updateSession = (sessionData: Session, column: string) => {
+  console.log(sessionData);
+  console.log(column);
+  axios
+    .put(`${serverPath}/api/sessions`, { data: sessionData, column })
+    .then((response) => {})
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const UTCtoPacific = (time: string) => {
+  const utcDateString = time;
+  const utcDate = new Date(utcDateString);
+
+  // Convert to Pacific Time
+  const options = { timeZone: 'America/Los_Angeles' };
+  const pacificDateString = utcDate.toLocaleString('en-US', options);
+
+  return pacificDateString;
+};
+
+// const convertAllTimes= (sessionArray:[])=>{
+//     for (let i = 0; i < sessionArray.length; i++) {
+// sessionArray[i].date_time = UTCtoPacific(sessionArray[i].date_time)
+//     }
+// }
+
+export { getSessions, postSession, deleteSession, updateSession };
