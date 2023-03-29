2eae447f5792b4fcf0746271bf5b2025daa1e13d
matvimykula <matt.pronchick@gmail.com>
2023-03-28 18:57:49 -0700

    change session type

--- src/types/session.ts
+++ src/types/session.ts
@@ -1,12 +1,11 @@
 interface Session {
   id?: string;
   client_id: string; // should be able to find client with matching unique
-
   reminder_sent: boolean;
   confirmed: boolean;
   canceled: boolean;
   location: string;
-  dateTime: Date;
+  date_time: Date;
 }
 
 export default Session;
